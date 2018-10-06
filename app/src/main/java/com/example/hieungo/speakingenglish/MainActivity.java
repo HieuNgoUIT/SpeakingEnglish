b95804bd3b20ae41924654da4d36012eceaef5a1
HieuNgo <hieungo1898@gmail.com>
2018-10-06 21:11:26 +0700

    test2

--- app/src/main/java/com/example/hieungo/speakingenglish/MainActivity.java
+++ app/src/main/java/com/example/hieungo/speakingenglish/MainActivity.java
@@ -1,13 +1,52 @@
 package com.example.hieungo.speakingenglish;
 
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
- //asdffdasfasdf
+    VideoView videoView;
+    ListView listView;
+    ArrayList<String> videoList;
+    ArrayAdapter adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        videoView=findViewById(R.id.videoview);
+        listView=findViewById(R.id.lvideo);
+        videoList= new ArrayList<>();
+        videoList.add("videotest");
+        videoList.add("videotest2");
+
+
+        adapter= new ArrayAdapter(this,android.R.layout.simple_list_item_1,videoList);
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                switch (position) {
+                    case 0:
+                        videoView.setVideoURI(Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.videotest));
+                        break;
+                    case 1:
+                        videoView.setVideoURI(Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.videotest2));
+                        break;
+                    default:
+                        break;
+                }
+                videoView.setMediaController(new MediaController(MainActivity.this));
+                videoView.requestFocus();
+                videoView.start();
+            }
+        });
     }
 }
